f25c02c6812d5466ebf91168fb81270ba805c2fd
geng <476878607@qq.com>
2017-12-12 17:53:40 +0800

    Revert "练习一下回退"
    This reverts commit 95dc424

--- src/com/yunzhs/crm/dao/AdminDao.java
+++ src/com/yunzhs/crm/dao/AdminDao.java
@@ -22,7 +22,4 @@ public class AdminDao {
 		String sql="insert into admin(adminname,password) values(?,?)";
 		qr.update(sql,admin.getAdminname(),admin.getPassword());
 	}
-    public void test(){
-        System.out.println("练习");
-    }
 }
